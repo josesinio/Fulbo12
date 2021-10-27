ac57c484c812a348f5ee712dd1c2b2c5d7649294
LucasMedina04 <medina.lucas.e@gmail.com>
2021-10-27 15:40:57 -0300

    Agregados Posicion y JuegaDe

--- src/Fulbo12.Core/Futbol/Futbolista.cs
+++ src/Fulbo12.Core/Futbol/Futbolista.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Fulbo12.Core.Futbol
 {
     public class Futbolista
@@ -5,6 +7,7 @@ namespace Fulbo12.Core.Futbol
         public Persona Persona { get; set; }
         public TipoFutbolista Tipofutbolista { get; set; }
         public Equipo Equipo { get; set; }
+        public List<Posicion> Pociciones { get; set;}
 
         public bool MismaNacionalidad(Futbolista futbolista)
             => Persona.MismaNacionalidad(futbolista.Persona);
@@ -14,5 +17,13 @@ namespace Fulbo12.Core.Futbol
         
         public bool MismoEquipo(Futbolista futbolista)
             => futbolista.Equipo == this.Equipo; 
+        
+        public bool JuegaDe(Posicion posicion)
+            => Pociciones.Contains(posicion);
+        
+        public Futbolista()
+        {
+            Pociciones = new List<Posicion>();
+        }
     }
 }
---
+++ src/Fulbo12.Core/Futbol/Posicion.cs
@@ -0,0 +1,8 @@
+namespace Fulbo12.Core.Futbol
+{
+    public class Posicion
+    {
+        public byte Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
