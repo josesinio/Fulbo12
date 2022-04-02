0c2b1f6b85431a2a9cac03d1a75b30c94eeab30d
LucasMedina04 <medina.lucas.e@gmail.com>
2022-04-01 23:16:24 -0300

    Agregadas 2 propiedades
    Propiedades agregadas:
    Linea:  CantidadPosiciones
    Formacion:  FormacionString

--- src/Fulbo12.Core/Formacion/Formacion.cs
+++ src/Fulbo12.Core/Formacion/Formacion.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Fulbo12.Core.Formacion
 {
@@ -12,6 +13,15 @@ namespace Fulbo12.Core.Formacion
         public byte QuimicaJugadores
         {
             get => Convert.ToByte(Lineas.Sum(item => item.QuimicaJugadores));
+        }
+        public string FormacionString
+        {
+            get
+            {
+                List<byte> aux = new List<byte>();
+                foreach (var linea in Lineas) aux.Add(linea.CantidadPosiciones);
+                return new StringBuilder().AppendJoin(" - ", aux).ToString();
+            }
         }
     }
 }
