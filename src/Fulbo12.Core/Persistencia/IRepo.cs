ec52ae104de73f0c38d749e8c94003a9cc92616d
Luis Duran <neoluis87@gmail.com>
2022-06-12 20:38:17 -0300

    Creada interfaz generica de Repo

---
+++ src/Fulbo12.Core/Persistencia/IRepo.cs
@@ -0,0 +1,11 @@
+using System.Linq.Expressions;
+
+namespace Fulbo12.Core.Persistencia;
+public interface IRepo<T> where T : class
+{
+    void Alta(T entidad);
+    IEnumerable<T> Obtener(
+        Expression<Func<T, bool>> filtro = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orden = null,
+        string includes = null);
+}
---
+++ src/Fulbo12.Core/Persistencia/IRepoPais.cs
@@ -0,0 +1,6 @@
+namespace Fulbo12.Core.Persistencia;
+public interface IRepoPais: IRepo<Pais>
+{
+
+
+}
