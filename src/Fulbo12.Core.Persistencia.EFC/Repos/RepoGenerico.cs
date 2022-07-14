96fc647b16ef1da4dac436b3ed0a45fdf37a5c61
luchoxx87 <ar7ur0321@gmail.com>
2022-07-14 15:45:05 -0300

    Update RepoGenerico.cs

--- src/Fulbo12.Core.Persistencia.EFC/Repos/RepoGenerico.cs
+++ src/Fulbo12.Core.Persistencia.EFC/Repos/RepoGenerico.cs
@@ -5,10 +5,10 @@ public abstract class RepoGenerico<T> : IRepo<T> where T : class
 {
     Fulbo12Contexto _contexto;
     public RepoGenerico(Fulbo12Contexto contexto) => _contexto = contexto;
-    public void Alta(T entidad) => _contexto.Set<T>().AddRange(entidad);
+    public void Alta(T entidad) => _contexto.Set<T>().Add(entidad);
     public void Alta(IEnumerable<T> entidades) => _contexto.Set<T>().AddRange(entidades);
     public async Task AltaAsync(T entidad)
-        => await _contexto.Set<T>().AddRangeAsync(entidad);
+        => await _contexto.Set<T>().AddAsync(entidad);
     public IEnumerable<T> Obtener(Expression<Func<T, bool>> filtro = null!, Func<IQueryable<T>, IOrderedQueryable<T>> orden = null!, string includes = null!)
     {
         throw new NotImplementedException();
