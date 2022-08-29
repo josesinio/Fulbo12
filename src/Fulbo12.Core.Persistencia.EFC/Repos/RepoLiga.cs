164a17b82afcabf1cee8bf6cf1dce45053e71e83
luchoxx87 <neoluis87@gmail.com>
2022-08-29 15:41:37 -0300

    Agregado RepoLiga.LigasDe(Pais)

--- src/Fulbo12.Core.Persistencia.EFC/Repos/RepoLiga.cs
+++ src/Fulbo12.Core.Persistencia.EFC/Repos/RepoLiga.cs
@@ -4,4 +4,12 @@ namespace Fulbo12.Core.Persistencia.EFC.Repos;
 public class RepoLiga : RepoGenerico<Liga>, IRepoLiga
 {
     public RepoLiga(Fulbo12Contexto contexto) : base(contexto) { }
+
+    public IEnumerable<Liga> LigasDe(Pais pais)
+        => Obtener( filtro: l => l.Pais == pais,
+                    orden: ls => ls.OrderBy(l => l.Nombre));
+
+    public async Task<IEnumerable<Liga>> LigasDeAsync(Pais pais)
+        => await ObtenerAsync(  filtro: l => l.Pais == pais,
+                                orden: ls => ls.OrderBy(l => l.Nombre));
 }
