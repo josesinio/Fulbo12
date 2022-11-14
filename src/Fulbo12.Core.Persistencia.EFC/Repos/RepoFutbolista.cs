32153aa7ccea6f2d779ea822724a695da8ed0714
NicolasBenitez30 <nicolasbenitez1438@gmail.com>
2022-11-14 17:08:45 -0300

    Update RepoFutbolista.cs

--- src/Fulbo12.Core.Persistencia.EFC/Repos/RepoFutbolista.cs
+++ src/Fulbo12.Core.Persistencia.EFC/Repos/RepoFutbolista.cs
@@ -5,4 +5,12 @@ public class RepoFutbolista : RepoGenerico<Futbolista>, IRepoFutbolista
 {
     public RepoFutbolista(Fulbo12Contexto contexto) : base(contexto) { }
     
+    public bool ExisteFutbolistaCon(byte idPersona, byte idTipoFutbolista, byte idEquipo)
+        => DbSet.Any(   f => EF.Property<byte>(f, "idPersona") == idPersona
+                    &&  EF.Property<byte>(f, "idEquipo") == idEquipo
+                    &&  EF.Property<byte>(f, "idTipoFutbolista") == idTipoFutbolista);
+    public async Task<bool> ExisteFutbolistaConAsync(byte idPersona, byte idTipoFutbolista, byte idEquipo) 
+        => await DbSet.AnyAsync(  f =>  EF.Property<byte>(f, "idPersona") == idPersona
+                    &&  EF.Property<byte>(f, "idEquipo") == idEquipo
+                    &&  EF.Property<byte>(f, "idTipoFutbolista") == idTipoFutbolista);
 }
