774aea768325c496b2fba4064a2e1aab52324ca1
josesinio <jose.cruzet12d1@gmail.com>
2023-06-26 16:12:57 -0300

    Area de jugador

--- src/Fulbo12.Core/Formacion/PosicionFormacion.cs
+++ src/Fulbo12.Core/Formacion/PosicionFormacion.cs
@@ -4,17 +4,23 @@ namespace Fulbo12.Core.Formacion;
 
 public class PosicionFormacion
 {
-    public int IdFormacion {get; set;}
-    public byte  NumCamiseta {get; set;}
-    public Futbolista Futbolista {get; set;}
-    public byte NroAlineacion {get; set;}
+    public int IdFormacion { get; set; }
+    public byte NumCamiseta { get; set; }
+    public Futbolista Futbolista { get; set; }
+    public byte NroAlineacion { get; set; }
+    public float PosicionX { get; set; }
+    public float PosicionY { get; set; }
+    public float Diagonal { get; set; }
 
-    public PosicionFormacion(int Idformacion, byte numCamiseta,Futbolista futbolista, byte nroAlineacion)
+    public PosicionFormacion(int Idformacion, byte numCamiseta, Futbolista futbolista, byte nroAlineacion, float posicionX, float posicionY, float diagonal)
     {
         IdFormacion = Idformacion;
-        NumCamiseta =numCamiseta;
-        NroAlineacion = nroAlineacion; 
-        Futbolista = futbolista; 
+        NumCamiseta = numCamiseta;
+        NroAlineacion = nroAlineacion;
+        Futbolista = futbolista;
+        PosicionX = posicionX;
+        PosicionY = posicionY;
+        Diagonal = diagonal;
     }
 
 
